1bd5906c984f8a37732da49367e548ec378fb0d2
camilaori <camila.ori.delgado@gmail.com>
2023-10-24 19:46:44 -0400

    Dockerfile, txt, primera parte del .py

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, jsonify, request, make_response
+from setuptools import setup, find_packages
+from os import environ
+from flask_sqlalchemy import SQLAlchemy
+from flask.views import MethodView
+from sqlalchemy.orm import clear_mappers
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@postgres-db/delgado_gonzalez'
+
+db= SQLAlchemy(app)
+
+class User (db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    emails = db.Column(db.ARRAY (db.String), nullable=False, unique=True)
+
+    def serialize (self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'emails': list(self.emails)
+        }
+    
+with app.app_context():
+    db.create_all()
+
+@app.route('/status/', methods= ['GET'])
+def getstatus():
+    return jsonify({'response': 'pong'})
+
+@app.route('/directories/', methods= ['GET'])
+def getdirectories():
+    user = User.query.all()
+    serialize_directories = [user.serialize() for user in user]
+    response = jsonify (serialize_directories)
+    return make_response (response, 200)
+
+@app.route('/directories/', methods= ['POST'])
+def postdirectories():
+    data = request.get_json()
+    name = data.get('name')
+    emails = data.get('emails')
+    user = User(name=name, emails=emails)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify(user.serialize())
+
+
+
+if __name__ == '__main__':
+    clear_mappers()
+    app.run()
